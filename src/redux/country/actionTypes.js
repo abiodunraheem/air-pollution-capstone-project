f3b5879e66a2c8b4e84714318b8c203f3cc0f1f7
abiodunraheem <raheemabiodun23@gmail.com>
2022-06-29 10:54:01 +0100

    Update aountry.js file

--- src/redux/country/actionTypes.js
+++ src/redux/country/actionTypes.js
@@ -1,3 +1,3 @@
-export const SEARCH_COUNTRY = 'space/country/SEARCH_COUNTRY';
+export const LOAD_COUNTRY = 'space/country/LOAD_COUNTRY';
 export const SELECT_COUNTRY = 'space/country/SELECT_COUNTRY';
 export const CANCEL_COUNTRY = 'space/country/CANCEL_COUNTRY';
--- src/redux/country/countrySlice.js
+++ src/redux/country/countrySlice.js
@@ -1,19 +1,47 @@
-import { SEARCH_COUNTRY, SELECT_COUNTRY, CANCEL_COUNTRY } from './actionTypes';
+import { LOAD_COUNTRY, SELECT_COUNTRY, CANCEL_COUNTRY } from './actionTypes';
 
 const initialState = [];
 
+const baseURL = 'http://api.openweathermap.org/data/2.5/air_pollution/history?lat=508&lon=50&start=1606223802&end=1606482999&appid={API key}';
+
+// action creator
+export const selectCountry = (id) => async (dispatch) => {
+  try {
+    return dispatch({ type: SELECT_COUNTRY, payload: id });
+  } catch (err) {
+    return err;
+  }
+};
+
+export const cancelCountry = (id) => async (dispatch) => {
+    try {
+      return dispatch({ type: CANCEL_COUNTRY, payload: id });
+    } catch (err) {
+      return err;
+    }
+  };
+
 // reducer function
 const countryReducer = (state = initialState, action) => {
-    // switch (action.type) {
-    //     case SEARCH_COUNTRY:
-    //         return payload.action
-    //     case SELECT_COUNTRY:
-    //         return payload.action
-    //     case CANCEL_COUNTRY:
-    //         return payload.action  
-    //     default:
-    //         return state;
-    // }
+    switch (action.type) {
+        case LOAD_COUNTRY:
+            return payload.action
+        case SELECT_COUNTRY:
+            return payload.action
+        case CANCEL_COUNTRY:
+            return payload.action  
+        default:
+            return state;
+    }
 }
+
+export const getCountryFromAPI = () => (dispatch) => fetch(baseURL)
+  .then((res) => res.json()).then((data) => {
+    const countries = data.map((country) => ({
+      id: country.id,
+      description: country.description,
+    }));
+    dispatch({ type: LOAD_COUNTRY, payLoad: countries });
+  }).catch(() => { });
 
 export default countryReducer;
